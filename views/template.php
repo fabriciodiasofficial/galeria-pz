0204ab6bf01ee86131f9e2ecd35363cda049b1e3
FABRICIO DIAS <fabriciodias97.ti@gmail.com>
2024-02-24 15:07:18 -0300

    Subindo galeria de fotos

---
+++ views/template.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Galeria de Fotos</title>
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/assets/css/style.css" />
+		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/javascript.js" ></script>
+	</head>
+	
+	<body>
+		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
+	</body>
+</html>
